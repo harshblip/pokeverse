0db7e1851d51c3daf623da8a33c9fd510464098a
harshblip <harshjipkate@gmail.com>
2023-06-07 19:26:28 +0530

    removed and added updated scss files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Footer from "./sections/Footer";
+import Wrapper from "./sections/Wrapper";
+import Background from "./components/Background";
+import './scss/index.scss';
 
 export default function App() {
   return (
-    <div> App </div>
-  )
+    <div className="main-container">
+      <Background />
+      <div className="app">
+        <Navbar />
+        <Wrapper />
+        <Footer />
+      </div>
+    </div>
+  );
 }
